96e562b808bf1530cc92b310f85fd51f3673a5c2
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-02-08 12:54:31 +0400

    Creating Layout component

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,10 @@
 
 // import Head from 'next/head';
 import Link from 'next/link';
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Home page</h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
@@ -26,7 +23,6 @@ export default function Home() {
       <Link href='/ninjas'>
         <a>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   );
 }
