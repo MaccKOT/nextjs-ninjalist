1f5df0773410817788bc09d714b217976ecdb9d3
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-02-08 12:34:50 +0400

    Navbar and Footer component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 //index.js for root path '/'
 
-import Head from 'next/head';
+// import Head from 'next/head';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Home page</h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
@@ -18,6 +21,8 @@ export default function Home() {
         corporis rerum voluptas nisi hic esse reprehenderit, quo est quas
         dolorum praesentium et.
       </p>
+
+      <Footer />
     </div>
   );
 }
