fa8bf511778ff80baf8cc70b1ce8c2f09ec56850
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-02-08 13:12:25 +0400

    Adding styles

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,19 @@
 //index.js for root path '/'
 
-// import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      <h1>Home page</h1>
-      <p>
+      <h1 className={styles.title}>Home page</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
         alias dolore ipsa earum perspiciatis explicabo, quaerat similique
         corporis rerum voluptas nisi hic esse reprehenderit, quo est quas
         dolorum praesentium et.
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
         alias dolore ipsa earum perspiciatis explicabo, quaerat similique
         corporis rerum voluptas nisi hic esse reprehenderit, quo est quas
@@ -21,7 +21,7 @@ export default function Home() {
       </p>
 
       <Link href='/ninjas'>
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </div>
   );
