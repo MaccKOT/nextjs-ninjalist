88fa5a6d1de85aba456018eed2e90fcbfb0f3ad1
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-02-08 14:12:12 +0400

    Fetching data with getStaticProps

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,31 @@
 // route is /ninjas/
 
-const Ninjas = () => {
+import styles from '../../styles/Ninjas.module.css';
+
+//function runs at buildtime
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const data = await res.json();
+
+  return {
+    props: {
+      ninjas: data,
+    },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
     <div>
       <h1>All Ninjas</h1>
+
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
