b088decf65c8f93621d9062fd76f360c3f72db28
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-02-08 13:26:55 +0400

    Redirecting user to home page from 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,18 @@
 import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // console.log('useEffect run');
+    setTimeout(() => {
+      // router.go(-1) - redirect user to prevoius page
+      router.push('/');
+    }, 3000);
+  }, []);
+
   return (
     <div className='not-found'>
       <h1>Ooooops...</h1>
